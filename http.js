35c03559e20fb71f36ed7556ef44638a7ff5b6c6
SoloJuan1 <jclmh@outlook.es>
2022-12-06 11:21:42 -0600

    Repasando Temas

--- http.js
+++ http.js
@@ -1,4 +1,4 @@
-const http = require('http')
+const http = require('http')    //es de node
 const router = require('./router.js')
 require('dotenv').config()
 
@@ -26,3 +26,4 @@ const server = http.createServer(function(peticion,respuesta){
 server.listen(process.env.PORT,process.env.HOST,function(){
     console.log(`Servidor disponible http://${process.env.HOST}:${process.env.PORT}`)
 })
+//127.0.0.1 es lo mismo que localhost
